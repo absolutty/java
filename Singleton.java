4a14dac862bbd24f575aa0f24ecf40c62cf2402e
absolutty <61381312+absolutty@users.noreply.github.com>
2020-07-12 17:06:32 +0200

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Singleton {
+    public String str;
+    private static final Singleton obj = new Singleton();
+
+    private Singleton(){}
+    
+    static Singleton getInstance(){
+        return obj;
+    }
+}
