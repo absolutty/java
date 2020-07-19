f61eda6e67f5765968027adab0bfa338d871fa68
Adam Hajro <61381312+absolutty@users.noreply.github.com>
2020-07-19 21:40:22 +0200

    Create DataTypes.java

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,6 @@
+byte b = Byte.MAX_VALUE; //od -128 do 127
+short s = Short.MAX_VALUE; //od -32768 do 32767
+int i = Integer.MAX_VALUE; //od 2,147,483,648 do 2,147,483,647
+long l = Long.MAX_VALUE; //od -9,223,372,036,854,775,808 do 9,223,372,036,854,775,807
+float f = Float.MAX_VALUE; //od 3.4028235E38 do 1.4E-45
+double d = Double.MAX_VALUE; //od 1.7976931348623157E308 do 4.9E-324
