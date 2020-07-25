67f4b7fe6da6bef6fa523a20dbb3095c46ff905c
Adam Hajro <61381312+absolutty@users.noreply.github.com>
2020-07-25 13:43:17 +0200

    Create MethodOverloading.java

---
+++ MethodOverloading/MethodOverloading.java
@@ -0,0 +1,11 @@
+public class Cisla {
+    public static int scitaj(int a, int b){
+        return a+b;
+    }
+    public static int scitaj(int a, int b, int c){
+        return a+b+c;
+    }
+    public static int scitaj(int a, int b, int c, int d){
+        return a+b+c+d;
+    }
+}
