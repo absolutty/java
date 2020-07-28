9dc8a04ff1bf44666799f2969d035a4689dcd463
Adam Hajro <61381312+absolutty@users.noreply.github.com>
2020-07-28 13:39:58 +0200

    Pridaný Binary Search na nájdenie sedadla.
    Zvýšená rýchlosť hľadania

--- Divadlo/Divadlo.java
+++ Divadlo/Divadlo.java
@@ -1,18 +1,21 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 import com.example.funkcie.Rozdelenie;
 
 public class Divadlo {
    private final String nazovDivadla;
-   private final List<Sedadlo> zoznamSedadiel = new ArrayList<>();
+   private final List<Sedadlo> zoznamSedadiel;
 
    //písmeno ktorým začína označenie sedadiel
    private static final char PRVE_PISMENO = 'A';
 
     public Divadlo(String nazovDivadla, int pocetRadov, int pocetSedadielRadu) {
         this.nazovDivadla = nazovDivadla;
+        this.zoznamSedadiel = new ArrayList<>();
 
         //generuje (pocetRadov * pocetSedadielRadu) sedadiel
         generujSedadla(pocetRadov, pocetSedadielRadu);
@@ -56,19 +59,15 @@ public class Divadlo {
 
     //funkcia na rezerváciu sedadla podľa jeho čísla
     public void rezervuj(String cisloSedadla){
-        boolean najdene = false;
+        Sedadlo hladaneSedadlo = new Sedadlo(cisloSedadla);//skúšobný objekt pre použitie compareTo
 
-        //cyklus na nájdenie daného sedadla
-        for(Sedadlo sedadlo : zoznamSedadiel){
-            //ak bolo nájdené
-            if(sedadlo.cisloSedadla.equals(cisloSedadla)){
-                najdene = true;
-                sedadlo.zarezervuj();
-            }
+        //vráti hodnotu s pozíciou sedadla, ak nebolo nájdené vráti -1
+        int najdeneSedadloPoz = Collections.binarySearch(zoznamSedadiel, hladaneSedadlo, null);
+
+        if(najdeneSedadloPoz >= 0){
+            zoznamSedadiel.get(najdeneSedadloPoz).zarezervuj();
         }
-
-        //ak je boolean najdene false, sedadlo nebolo najdene
-        if(!najdene){
+        else{
             Rozdelenie.vypis();
             System.out.println("Sedadlo s číslom " + cisloSedadla + " nebolo nájdené.");
         }
@@ -76,19 +75,15 @@ public class Divadlo {
 
     //funkcia na zrušenie rezervácie sedadla podľa jeho čísla
     public void zrusRezervaciu(String cisloSedadla){
-        boolean najdene = false;
+        Sedadlo hladaneSedadlo = new Sedadlo(cisloSedadla);//skúšobný objekt pre použitie compareTo
 
-        //cyklus na nájdenie daného sedadla
-        for(Sedadlo sedadlo : zoznamSedadiel){
-            //ak bolo nájdené
-            if(sedadlo.cisloSedadla.equals(cisloSedadla)){
-                najdene = true;
-                sedadlo.odrezervuj();
-            }
+        //vráti hodnotu s pozíciou sedadla, ak nebolo nájdené vráti -1
+        int najdeneSedadloPoz = Collections.binarySearch(zoznamSedadiel, hladaneSedadlo, null);
+
+        if(najdeneSedadloPoz >= 0){
+            zoznamSedadiel.get(najdeneSedadloPoz).odrezervuj();
         }
-
-        //ak je boolean najdene false, sedadlo nebolo najdene
-        if(!najdene){
+        else{
             Rozdelenie.vypis();
             System.out.println("Sedadlo s číslom " + cisloSedadla + " nebolo nájdené.");
         }
@@ -98,7 +93,7 @@ public class Divadlo {
         return nazovDivadla;
     }
 
-    private static class Sedadlo{
+    private static class Sedadlo implements Comparable<Sedadlo>{
        private final String cisloSedadla;
        private boolean zarezervovane = false;
 
@@ -111,7 +106,12 @@ public class Divadlo {
            this.cisloSedadla = cisloSedadla;
        }
 
-       private void zarezervuj(){
+        @Override // porovnáva na základe jeho cisla (cisloSedadla)
+        public int compareTo(Sedadlo sedadlo) {
+            return this.cisloSedadla.compareToIgnoreCase(sedadlo.cisloSedadla);
+        }
+
+        private void zarezervuj(){
            Rozdelenie.vypis();
 
            if(!this.zarezervovane){
