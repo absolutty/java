5a619388267921f5675c5b31bc7ce39ba2d88539
Adam Hajro <61381312+absolutty@users.noreply.github.com>
2020-09-17 10:59:30 +0200

    Add files via upload

---
+++ Generics/Players/BaseballPlayer.java
@@ -0,0 +1,9 @@
+package com.company.players;
+
+import com.company.Player;
+
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String menoHraca) {
+        super(menoHraca);
+    }
+}
---
+++ Generics/Players/SoccerPlayer.java
@@ -0,0 +1,9 @@
+package com.company.players;
+
+import com.company.Player;
+
+public class SoccerPlayer extends Player {
+    public SoccerPlayer(String menoHraca) {
+        super(menoHraca);
+    }
+}
