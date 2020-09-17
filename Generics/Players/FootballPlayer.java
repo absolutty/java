a0233ec31ccfd56bf57af4eee7db87bff05ea6e0
Adam Hajro <61381312+absolutty@users.noreply.github.com>
2020-09-17 10:59:14 +0200

    Create FootballPlayer.java

---
+++ Generics/Players/FootballPlayer.java
@@ -0,0 +1,9 @@
+package com.company.players;
+
+import com.company.Player;
+
+public class FootballPlayer extends Player {
+    public FootballPlayer(String menoHraca) {
+        super(menoHraca);
+    }
+}
